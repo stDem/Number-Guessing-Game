da3469552afdcc2909b4481df22a4305e95bde30
Stef Altavista <stefanomsc@hotmail.it>
2025-01-28 11:29:17 +0100

    restructure and task assignment

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
+const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
 
+game();
 
-const generateRandomNumber = () => Math.integer(Math.random() * 100) + 1;
+// ELIF
+const game = (points) => {
+    startGame = confirm(
+        "Hello, guys! I am the evil AI once more, and this time I challenge you to a game of wits: a Number Guessing Game. Good luck, and may the best coder win! Mwahahaha!"
+    );
 
-const getPlayerGuess = () => { 
-  playerGuess = prompt("Enter a number between 1 and 100", '');
+    if (startGame) {
+        let gameResult = play();
+        endGame();
+    } else {
+        open("./images/coward.jpg", "_self");
+    }
+};
 
-}
+// STEF
+const play = () => {
+    randomNumber = generateRandomNumber();
+    countAttempts = 0;
 
-const checkGuess = (playerGuess, correctNumber) => { 
+    // while - Use a loop to repeatedly prompt the player for guesses until they
+    // guess correctly or use up a maximum of 10 attempts
 
-  return "too low, too high, or correct";
-}
+    // return an object {attempts: number , victory:boolean}
+    print("won or lost");
+    print(`Nuber of attempts: ${countAttempts}`);
+};
 
-const game = () => { 
-  randomNumber = generateRandomNumber();
-  countAttempts = 0;
+// ANASTASIIA
+const endGame = (gameResult) => {
+    // Bonus Challenge: Implement a scoring system that rewards the player with
+    // points based on how quickly they guess the correct number
 
-  // while - Use a loop to repeatedly prompt the player for guesses until they
-  // guess correctly or use up a maximum of 10 attempts
+    if (gameResult.vicotry) {
+        let points = 10 - gameResult.attempts + gameResult.points;
+        //   you won and made a total of ${points} points
+        // wanna play again?
+        game(points);
+    } else {
+        // you lost
+        // try again?
+    }
+};
 
-  print("won or lost");
-  print(`Nuber of attempts: ${countAttempts}`);
+// Helper Functions
+// STEF
+const getPlayerGuess = () => {
+    let playerGuess = prompt("Enter a number between 1 and 100", "");
+    // check that player entered a valid number
+};
 
-
-}
-
-// Bonus Challenge: Implement a scoring system that rewards the player with
-// points based on how quickly they guess the correct number
-
-startGame = confirm("Hello, guys! I am the evil AI once more, and this time I challenge you to a game of wits: a Number Guessing Game. Good luck, and may the best coder win! Mwahahaha!");
-
-if (startGame) {
-  // main game logic
-} else {
-  open("./coward.jpg");
-}
-
-
+// ELIF
+const checkGuess = (playerGuess, correctNumber) => {
+    // Switch
+    // return string L,H,C
+    return "too low, too high, or correct";
+};
