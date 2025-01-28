fe544b1f38e0da545c33f0918ee18d24668bb6c8
Stef Altavista <stefanomsc@hotmail.it>
2025-01-28 14:49:17 +0100

    input check and game logic implemented

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
 
-
-
 // ELIF
 const game = (points) => {
     startGame = confirm(
@@ -10,23 +8,54 @@ const game = (points) => {
 
     if (startGame) {
         let gameResult = play();
-        endGame();
+        console.log(gameResult);
+        // endGame();
     } else {
-        open("./images/coward.jpg", "_self");
+        open("./coward.jpg", "_self");
     }
 };
 
 // STEF
 const play = () => {
-    randomNumber = generateRandomNumber();
-    countAttempts = 0;
+    const selectedNumber = generateRandomNumber();
+    let attempts = 0;
+    let victory = false;
+    let playerGuess;
 
-    // while - Use a loop to repeatedly prompt the player for guesses until they
-    // guess correctly or use up a maximum of 10 attempts
+    do {
+        attempts++;
+        attempts == 10 ? alert("LAST CHANCE!") : null;
+        playerGuess = getPlayerGuess();
+        console.log(`Attempt n. ${attempts}\nPlayer guess: ${playerGuess}`);
+        let check = checkGuess(playerGuess, selectedNumber);
 
-    // return an object {attempts: number , victory:boolean}
-    print("won or lost");
-    print(`Nuber of attempts: ${countAttempts}`);
+        switch (check) {
+            case "L":
+                attempts != 10
+                    ? alert(
+                          `Wrong Guess \nYour Guess is lower than My number! \nYou have ${
+                              10 - attempts
+                          } attempts left `
+                      )
+                    : alert(`Sorry... no luck for you today`);
+                break;
+            case "H":
+                attempts != 10
+                    ? alert(
+                          `Wrong Guess \nYour Guess is higher than My number! \nYou have ${
+                              10 - attempts
+                          } attempts left `
+                      )
+                    : alert(`Sorry... no luck for you today`);
+                break;
+            case "C":
+                victory = true;
+                alert(`Correct!`);
+                break;
+        }
+    } while (attempts < 10 && !victory);
+
+    return { attempts, victory };
 };
 
 // ANASTASIIA
@@ -45,18 +74,54 @@ const endGame = (gameResult) => {
     }
 };
 
-// Helper Functions
 // STEF
 const getPlayerGuess = () => {
-    let playerGuess = prompt("Enter a number between 1 and 100", "");
-    // check that player entered a valid number
+    let stringInput = userInput("Enter a number between 1 and 100", "");
+    const regex = /^[0-9]+$/;
+    let input;
+
+    if (regex.test(stringInput)) {
+        input = parseInt(stringInput, 10);
+
+        if (isNaN(input)) {
+            alert("Invalid input. Please enter only numeric characters.");
+            return getPlayerGuess();
+        } else if (input > 100) {
+            alert("The number is too high!");
+            return getPlayerGuess();
+        } else if (input <= 0) {
+            alert("The number is too low!");
+            return getPlayerGuess();
+        }
+    } else {
+        alert("Invalid input. Please enter only numeric characters.");
+        return getPlayerGuess();
+    }
+
+    return input;
 };
+
+function userInput(text, placeholder = "") {
+    let input = prompt(text, placeholder);
+    if (input == null) {
+        let exit = confirm("Do you wish to quite the game?");
+        if (exit) {
+            alert("Bye Bye");
+            return quit();
+        } else userInput(text, placeholder);
+    } else return input;
+}
+
+function quit() {
+    open("./coward.jpg", "_self");
+    throw new Error("some error");
+}
 
 // ELIF
 const checkGuess = (playerGuess, correctNumber) => {
     // Switch
     // return string L,H,C
-    return "too low, too high, or correct";
+    return "L";
 };
 
 game();
